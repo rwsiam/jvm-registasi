10742b7bc38566c198e0659e29036ba7e54ea9ca
Rwsiam <mac@rwsiam.local>
2020-03-07 14:10:25 +0700

    add layout

--- src/main/java/com/doku/aplikasiregistrasi/AplikasiRegistrasiApplication.java
+++ src/main/java/com/doku/aplikasiregistrasi/AplikasiRegistrasiApplication.java
@@ -1,13 +1,20 @@
 package com.doku.aplikasiregistrasi;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AplikasiRegistrasiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AplikasiRegistrasiApplication.class);
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 
 }
--- src/main/java/com/doku/aplikasiregistrasi/controller/RegistrasiController.java
+++ src/main/java/com/doku/aplikasiregistrasi/controller/RegistrasiController.java
@@ -2,6 +2,7 @@ package com.doku.aplikasiregistrasi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrasiController {
@@ -9,6 +10,11 @@ public class RegistrasiController {
     @GetMapping("/registrasi")
     public void registrasiForm(){
 
+    }
+
+    @PostMapping("/registrasi")
+    public String registrasiSubmit(){
+        return "redirect:konfirmasi";
     }
 
     @GetMapping("/konfirmasi")
