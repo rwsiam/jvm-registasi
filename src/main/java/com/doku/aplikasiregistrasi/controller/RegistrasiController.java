c70a51b1d007864fb60e2c54ae32116dbef4b885
Rwsiam <mac@rwsiam.local>
2020-03-07 16:37:28 +0700

    add build

--- src/main/java/com/doku/aplikasiregistrasi/controller/RegistrasiController.java
+++ src/main/java/com/doku/aplikasiregistrasi/controller/RegistrasiController.java
@@ -1,24 +1,47 @@
 package com.doku.aplikasiregistrasi.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @Controller
+@RequestMapping("/registrasi")
 public class RegistrasiController {
 
-    @GetMapping("/registrasi")
-    public void registrasiForm(){
+    @GetMapping("/form")
+    public ModelMap tampilkanFormRegistrasi() {
+        log.info("Menjalankan method tampilkanFormRegistrasi");
+        ModelMap mm = new ModelMap();
+        mm.addAttribute("nama", "Endy");
+        mm.addAttribute("waktu", LocalDateTime.now());
+        return mm;
+    }
+
+    @PostMapping("/form")
+    public String prosesFormRegistrasi() {
+        log.info("Seharusnya nanti di sini insert ke database");
+
+        /* jangan return html, return redirect supaya tidak dobel submit
+        ModelAndView mav = new ModelAndView("konfirmasi");
+        return mav;
+         */
+
+        return "redirect:confirmation";
+    }
+
+    @GetMapping("/confirmation")
+    public void tampilkanHalamanKonfirmasi() {
 
     }
 
-    @PostMapping("/registrasi")
-    public String registrasiSubmit(){
-        return "redirect:konfirmasi";
-    }
-
-    @GetMapping("/konfirmasi")
-    public void konfirmasiForm(){
+    @GetMapping("verified")
+    public void emailTerverifikasi() {
 
     }
 }
