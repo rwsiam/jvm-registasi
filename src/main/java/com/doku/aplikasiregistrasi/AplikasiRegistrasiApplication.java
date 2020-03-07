1ac353163accade0ddf998e8f054ba258a706bb0
Rwsiam <mac@rwsiam.local>
2020-03-07 10:27:59 +0700

    first commit

---
+++ src/main/java/com/doku/aplikasiregistrasi/AplikasiRegistrasiApplication.java
@@ -0,0 +1,13 @@
+package com.doku.aplikasiregistrasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiRegistrasiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplikasiRegistrasiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doku/aplikasiregistrasi/AplikasiRegistrasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doku.aplikasiregistrasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiRegistrasiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
